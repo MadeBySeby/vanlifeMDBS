80c45818ab4fae722a9da3adf46ee0b2d79327fb
dato sebiskveradze <david.katsitadze.1@btu.edu.ge>
2025-05-15 00:09:42 +0400

    added vans and van details, iam not fixing css and responsive bugs at this time

--- vanlifemadebyseby/src/main.jsx
+++ vanlifemadebyseby/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 // import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom/client";
-
 import App from "./App.jsx";
+import "./server";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
--- vanlifemadebyseby/src/pages/Vans/VanDetail.jsx
+++ vanlifemadebyseby/src/pages/Vans/VanDetail.jsx
@@ -1,5 +1,55 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams, Link } from "react-router-dom";
+import { getVans } from "../../api";
 export default function VanDetail() {
-  return <div>VanDetail</div>;
+  //   const location = useLocation();
+  const [van, setVan] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    async function loadVan() {
+      setLoading(true);
+      try {
+        const data = await getVans(id);
+        setVan(data);
+        console.log(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadVan();
+  }, [id]);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>There was an error: {error.message}</h1>;
+  }
+  return (
+    <div className="van-detail-container">
+      {/* <Link to={`..${search}`} relative="path" className="back-button"> */}
+      <Link to={`..`} relative="path" className="back-button">
+        &larr; <span>Back to all vans</span>
+        {/* &larr; <span>Back to {type} vans</span> */}
+      </Link>
+
+      {van && (
+        <div className="van-detail">
+          <img src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price">
+            <span>${van.price}</span>/day
+          </p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+      )}
+    </div>
+  );
 }
