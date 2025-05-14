53008c1a78799a3af9299f1e08256a935394abe1
dato sebiskveradze <david.katsitadze.1@btu.edu.ge>
2025-05-14 22:59:11 +0400

    added fake server for not dive into backend yet

--- vanlifemadebyseby/src/App.jsx
+++ vanlifemadebyseby/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans/Vans";
+import VanDetail from "./pages/Vans/VanDetail";
 // import Vans from "./pages/Vans";
 // import Host from "./pages/Host";
 // import VanDetail from "./pages/VanDetail";
@@ -15,6 +17,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          {/* <Route path="login" element={<About />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ vanlifemadebyseby/src/pages/Vans/VanDetail.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function VanDetail() {
+  return <div>VanDetail</div>;
+}
